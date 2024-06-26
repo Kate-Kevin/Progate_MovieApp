83d32b0d5072278090e418c9bdae740a1e3536b0
Kevin T <kevinkho14@gmail.com>
2024-06-26 15:57:10 +0700

    Home Navigation Compleate

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text , Button} from 'react-native'
 
-export default function Home(): JSX.Element {
+
+export default function Home({ navigation }): JSX.Element {
   return (
     <View>
-      <Text>Home</Text>
+      <Button title="Movie Detail" onPress={() => navigation.navigate('MovieDetail')} />
     </View>
   )
 }
